daf95c08bba44319dd42dbc69006e0e94c12d690
Vladislav <bukatinvladislav@gmail.com>
2015-02-23 20:23:47 +0200

    Revert "new"
    This reverts commit 16e1e15f96712aca42632d77fb3e1bf416205e9b.

--- src/main/java/Entity/SessionAttributes.java
+++
@@ -1,9 +0,0 @@
-package Entity;
-
-/**
- * Created by Vlad on 08.02.2015.
- */
-public class SessionAttributes {
-    public static final String PRODUCTS_IN_BUCKET = "productsInBucket";
-    private SessionAttributes(){}
-}
